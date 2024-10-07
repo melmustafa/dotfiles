55ee216778580dc873ad81a49b52597fd291963b
Mohammed El-Mustafa Ahmed <m.m.elhag.elamin@gmail.com>
2024-10-07 13:10:17 +0300

    update alacrity and neovim to latest

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -8,4 +8,26 @@ return {
     'Leviathenn/nvim-transparent',
     opts = { enable = true },
   },
+  {
+    'windwp/nvim-ts-autotag',
+    ft = {
+      'html',
+      'javascript',
+      'typescript',
+      'vue',
+    },
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
+  {
+    'olexsmir/gopher.nvim',
+    ft = 'go',
+    config = function(_, opts)
+      require('gopher').setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
 }
