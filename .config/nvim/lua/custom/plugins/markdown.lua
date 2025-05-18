a17d40c2baeec820b137e22f2ef0e8032e974439
Mohammed El-Mustafa Ahmed <m.m.elhag.elamin@gmail.com>
2025-05-18 13:04:16 +0300

    nvim: load markdown related plugins only when a markdown file is loaded

--- .config/nvim/lua/custom/plugins/markdown.lua
+++ .config/nvim/lua/custom/plugins/markdown.lua
@@ -5,11 +5,12 @@ return {
     opts = {},
     name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you use the mini.nvim suite
+    ft = { 'markdown' },
   },
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && COREPACK_ENABLE_AUTO_PIN=0 yarn install --no-lockfile',
+    build = 'cd app && yarn install --frozen-lockfile',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
     end,
