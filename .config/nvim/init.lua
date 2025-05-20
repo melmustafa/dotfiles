1e28e1978b927dd7cb056d7fd9e8802c01b200fc
Mohammed El-Mustafa Ahmed <m.m.elhag.elamin@gmail.com>
2025-05-20 12:15:53 +0300

    nvim: disbale autosuggestion unless asked for by ctrl-space

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -897,6 +897,7 @@ require('lazy').setup({
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        trigger = { show_on_keyword = false },
       },
 
       sources = {
